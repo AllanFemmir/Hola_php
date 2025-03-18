808cf7f047f74cef864c378ea8d7a6d9d212a57c
Allan Miranda <25008249@galileo.edu>
2025-03-18 03:02:51 +0000

    se añadio el archivo php

---
+++ hola.php
@@ -0,0 +1,11 @@
+<?php
+$nombre = "AllAN MIRANDA";  // String // Boolean
+
+echo "<h1> HOLA MUNDO MI NOMBRE ES:  $nombre, MI CARNE ES : 25008249 </h2>";
+$linea = "<br>";
+$imagen = '<img src = gatito.jpeg >';
+echo $imagen.$linea;
+;
+
+
+?>
